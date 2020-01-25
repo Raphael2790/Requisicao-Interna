c363ef83f1cfed75591398a29582cb89a564c3b9
Raphael2790 <rssshev@gmail.com>
2020-01-25 15:59:14 -0300

    Inciada a formatação css da pagina inicial

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,34 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
+
+/*function transformPDF(pagina) {
+  pdf.create(pagina).toFile('../pdf/Requisicao_Salva.pdf',(err,res) => {
+    if(err) {
+      console.log(err)
+    } else{
+      console.log(res)
+      }
+    }
+  )
+} */
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Requisição Web - San Raphael Hotel'});
+  const setores = [
+   'Cozinha',
+   'Restaurante', 
+   'Padaria', 
+   'Governanca', 
+   'Eventos', 
+   'Financeiro',
+   'Choperia']
+  res.render('index', { 
+    title: 'Requisição Web',
+    listaSetores: setores,
+    link: '/req',
+    imagem:'../images/logoSRH.png',
+});
 });
 
 module.exports = router;
